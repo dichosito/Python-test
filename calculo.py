04d11f2c629e2c26e66c10e34dd619686fb7c7fa
dichosito <74221354+dichosito@users.noreply.github.com>
2021-05-26 11:53:15 -0500

    ver original

--- calculo.py
+++ calculo.py
@@ -1,16 +1,20 @@
+import sys
+
 def Fibonacci(n):
-	if n>0:
-		a=1
-		b=1
-		if n==1:
-			print('0')
-		elif n==2:
-			print('0','1')
-		else:
-			for i in range(n-3):
-				total = a + b
-				b=a
-				a= total
-			print(total)
-			 
-Fibonacci(200)
+        if n<0:
+           sys.exit("El numero ingresado no es positivo")
+        else:    
+           a=1
+           b=1
+           if n==1:
+              print('0')
+           elif n==2:
+              print('0','1')
+           else:
+              for i in range(n-3):
+                total = a + b
+                b=a
+                a=total
+           print("Del número ",n,"su fibonacci es",total)
+                                      
+Fibonacci(-200)

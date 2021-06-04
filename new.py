7be1b513362c9a499463954ee7931ac92ad99a6c
dichosito <74221354+dichosito@users.noreply.github.com>
2021-06-04 16:12:45 -0500

    version update

--- new.py
+++ new.py
@@ -1,20 +1,36 @@
 import numpy as np
-import numpy.linalg as la
-N = 10 #1000
-p= 3
+import numpy.linalg as LA
+N = 1000 #No. de vectores aleatorios
+p = 3 #Dimension de matriz y vectores
 A = np.array([[1,2,3],
               [4,5,6]])
-
+print(A)
 def estimar_norma_mat(A,p,N):    # estimar norma p de matriz A usando N vectores aleatorios
     y = np.zeros(N)       # vector para almacenar el cociente entre las normas de vectores
-    print(y)
+       
     for i in range(N):        
         # escribe tu codigo aqui
-        random = np.random.randn(p)
-        print (random)
-        
-    return #np.max(y)
+        vector = np.random.randn(p)
+        if i < 11:
+            print("N",i,vector)
+        if i > N-10:
+            print("N",i,vector)
+        norma = LA.norm(A@vector,np.inf)/ LA.norm(vector,np.inf)
+        y[i-1] = norma
+
+    #print (y) 
+    print ("Maximo",np.max(y))
+    ordenes = [1,2,np.inf]
+    aproximado = [LA.norm(y,orden) for orden in ordenes]
+    real = [LA.norm(A,orden) for orden in ordenes]
+
+    print(f"{'':11}|{'||A||1':-^20}|{'||A||2':-^20}|{'||A||oo':-^20}")
+    print("|".join(["Aproximado "]+[f"{x:20}" for x in aproximado]))
+    print("|".join(["Exacto     "]+[f"{x:20}" for x in real]))
+    
+    return 
 
 # ejecuta tu codigo aqui
 estimar_norma_mat(A,p,N)
+
 
